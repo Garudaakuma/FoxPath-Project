eff7bd6ed87f1738603dc1777b5fdda51b976912
flaviacfagundes <flaviacrisfagundes@gmail.com>
2024-11-17 21:25:40 -0300

    update01

--- Site_em_si/SCRIPTS/PHP/login.php
+++ Site_em_si/SCRIPTS/PHP/login.php
@@ -3,48 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/style_main.css">
-    <link rel="stylesheet" href="../CSS/style_login.css">
+    <link rel="icon" type="image/x-icon" href="/FoxPath-Project/Site_em_si/IMAGES/ico_logoFoxPath/favicon.ico">
+    <link rel="stylesheet" href="/FoxPath-Project/Site_em_si/SCRIPTS/CSS/style_main.css">
+    <link rel="stylesheet" href="/FoxPath-Project/Site_em_si/SCRIPTS/CSS/style_login.css">
 
     <title>FOXPATH - Login</title>
 </head>
 <body class="tela_login">
     <section class="foxpath_intro">
         <div class="letters">F</div>
-            <img class="logo_foxpath" src="/FOXPATH/IMAGES/foxpath-transparent.png" alt="logo_foxpath">
+            <img class="logo_foxpath" src="/FoxPath-Project/Site_em_si/IMAGES/foxpath-transparent.png" alt="logo_foxpath">
             <div class="letters">XPATH</div>
     </section>
-    <section class="foxpath_faca_login">
-        <div class="sub_title_letters">FAÇA O LOGIN!</div>
+    <div class="body_Login">
+        <div class="sub_title_letters">FAÇA O LOGIN!</div> <br>
         <div class="insert_dados">
             <div class="EMAIL">
                 <div class="text_email">Email:</div>
                 <div class="insert_email">
-                    <input type="text" id="email" name="email" placeholder="DIGITE EMAIL AQUI">
+                    <input type="text" id="email" name="email" placeholder="DIGITE O EMAIL AQUI">
                 </div>
-                <div class="forgot_email">Esqueceu o Email?</div>
+                <div class="forgot_email"><a href="">Esqueceu o Email?</a></div> <br>
             </div>
             <div class="SENHA">
                 <div class="text_senha">Senha:</div>
                 <div class="insert_senha">
                     <input type="text" id="email" name="email" placeholder="DIGITE UMA SENHA AQUI">
                 </div>
-                <div class="forgot_senha"> Esqueceu a Senha?</div>
+                <div class="forgot_senha"><a href="">Esqueceu a Senha?</a></div> <br>
             </div>
             <div class="USUARIO">
                 <div class="text_usuario">Usuário:</div>
                 <div class="choose_user">
-                    <input type="checkbox" id="padrao" name="padrao"> PADRÃO
-                    <input type="checkbox" id="fluente" name="fluente"> FLUENTE
-                    <input type="checkbox" id="adm" name="adm"> ADMINISTRADOR
-                </div>
+                    <input type="checkbox" id="padrao" name="padrao">
+                    <label for="padrao">PADRÃO</label>
+                    <input type="checkbox" id="fluente" name="fluente">
+                    <label for="fluente">FLUENTE</label>
+                    <input type="checkbox" id="adm" name="adm">
+                    <label for="adm">ADMINISTRADOR</label>
+                </div> <br>
             </div>
         </div>
         <div class="login_buttons">
-            <div class="login_button"><button>Login</button></div>
+            <div class="login_button"><button>Login</button></div> <br>
             <div class="back_button"><button onclick="btn_pagina_inicial()">Voltar</button></div>
         </div>
-    </section>
+    </div>
 </body>
 <script src="../JS/main.js"></script>
 </html>
--- Site_em_si/SCRIPTS/PHP/usuario.php
+++ Site_em_si/SCRIPTS/PHP/usuario.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="/FOXPATH/IMAGES/ico_logoFoxPath/favicon.ico">
-    <link rel="stylesheet" href="../CSS/style_usuario.css">
+    <link rel="icon" type="image/x-icon" href="/FoxPath-Project/Site_em_si/IMAGES/ico_logoFoxPath/favicon.ico">
+    <link rel="stylesheet" href="/FoxPath-Project/Site_em_si/SCRIPTS/CSS/style_usuario.css">
     <title>FOXPATH - Usuário</title>
 </head>
 <header class="header_usuario">
