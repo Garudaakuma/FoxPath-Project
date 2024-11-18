ce9bd383341040067d8dd042b7bd25c8d16e5ff2
flaviacfagundes <flaviacrisfagundes@gmail.com>
2024-11-17 23:58:27 -0300

    update05

--- Site_em_si/SCRIPTS/PHP/adm.php
+++ Site_em_si/SCRIPTS/PHP/adm.php
@@ -13,7 +13,21 @@
     <button onclick="btn_pagina_inicial()" class="logout">Logout</button>
 </header>
 <body class="body_adm">
-    
+    <div class="menu_adm"> <br>
+        <div class="denuncias">
+            <button class="btn_denuncias">DENUNCIAS</button>
+        </div> <br>
+        <div class="perfis">
+            <button class="btn_perfis">PERFIS</button>
+        </div> <br>
+        <div class="banidos">
+            <button class="btn_banidos">BANIDOS</button>
+        </div> <br>
+        <div class="relatorio">
+            <button class="btn_relatorio">RELATÓRIO</button>
+        </div> <br>
+    </div>
+    <div class="conteudo"></div>
 </body>
 <script src="../JS/main.js"></script>
 </html>
