5555ecb5aa5723b486a4c2c1f836dffa35e496da
flaviacfagundes <flaviacrisfagundes@gmail.com>
2024-11-17 23:09:07 -0300

    update04

---
+++ Site_em_si/SCRIPTS/PHP/adm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/FoxPath-Project/Site_em_si/IMAGES/ico_logoFoxPath/favicon.ico">
+    <link rel="stylesheet" href="/FoxPath-Project/Site_em_si/SCRIPTS/CSS/adm.css">
+    <title>FOXPATH - Administrador</title>
+</head>
+<header class="header_adm">
+    <div class="icon_perfil">icon_perfil</div>
+    <div class="nome">nome</div>
+    <button onclick="btn_pagina_inicial()" class="logout">Logout</button>
+</header>
+<body class="body_adm">
+    
+</body>
+<script src="../JS/main.js"></script>
+</html>
