be03d591b48ef0dbf0534388442f071cd56768bf
Lucas Aioria Serpa <136469417+Garudaakuma@users.noreply.github.com>
2024-11-18 07:44:56 -0300

    reset cad_part in cad_save.json

--- Site_em_si/SCRIPTS/PHP/LOGIC/reset_cad.php
+++ Site_em_si/SCRIPTS/PHP/LOGIC/reset_cad.php
@@ -1,7 +1,7 @@
 
 <?php
-$jsonDecoded = json_decode(file_get_contents("/xampp/htdocs/FoxPath-Project/Site_em_si/JSON/bd_info.json"), true);
+$jsonDecoded = json_decode(file_get_contents("/xampp/htdocs/FoxPath-Project/Site_em_si/JSON/cad_save.json"), true);
 $jsonDecoded["cad_part"] = "1";
 $jsonEncoded = json_encode($jsonDecoded, JSON_PRETTY_PRINT);
-file_put_contents("/xampp/htdocs/FoxPath-Project/Site_em_si/JSON/bd_info.json", $jsonEncoded);
+file_put_contents("/xampp/htdocs/FoxPath-Project/Site_em_si/JSON/cad_save.json", $jsonEncoded);
 header("Location: /FoxPath-Project/Site_em_si/SCRIPTS/PHP/main.php");
